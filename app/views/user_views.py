089d544a8d1d8795219cd0b7c3040abfd6e50ab9
daxata06 <126562911+daxata06@users.noreply.github.com>
2024-03-05 12:51:21 +0300

    Update user_views.py

--- app/views/user_views.py
+++ app/views/user_views.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request, redirect, flash, get_flashed_
 from app.models.user import *
 from config import *
 from . import views
+from sqlalchemy.sql.expression import func
 
 
 @views.route('/')
 def index():
-    return render_template("home_page.html")
+    name_username = users.query.order_by(func.random()).limit(5).all()
+
+    avatars_urls = {user.id_: get_avatar(user.id_) for user in name_username}
+    return render_template("home_page.html",
+                           name_username=name_username,
+                           avatars_urls=avatars_urls)
 
